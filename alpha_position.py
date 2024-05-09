dd2a579bfc049034456b2ba23f960f13be56157f
botchedeternal <akearsey83@gmail.com>
2024-05-08 20:57:57 -0600

    alpha position long

--- alpha_position.py
+++ alpha_position.py
@@ -1,6 +1,7 @@
 #In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 #If anything in the text isn't a letter, ignore it and don't return it.
 
+
 def alphabet_position(text):
         txt = text.lower()
         letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
