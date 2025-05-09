1883f966a318caf27b743e8c1a6a048a4c521358
Adam Kearsey <104175971+8bitcommit@users.noreply.github.com>
2025-05-09 14:22:41 -0600

    split strings

--- split_string.py
+++ split_string.py
@@ -4,12 +4,10 @@ def solution(s):
         x=[]
         if (len(s)%2!=0):
                 s  = s+'_'
-        count=2
         for i in range(len(s)):
                 if (i%2==0):
-                        x.append(s[i:count])
-                        count+=2
-        print(x)
+                        x.append(s[i:i+2])
+        return(x)
 
 
 if __name__ == "__main__":
